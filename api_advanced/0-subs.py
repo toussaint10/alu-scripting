f825f6500d788eb9fc96142c206d18403f5e17b1
toussaint10 <l.tona@alustudent.com>
2024-04-06 10:17:33 -0700

    API Advanced

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""how many subs"""
+"""Return number of subscribers for a given subreddit"""
 import requests
 
 
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Return the number of subscribers """
     url = "https://www.reddit.com/r/{}/about.json" \
         .format(subreddit)
-    headers = {'User-Agent': 'My API advanced 1.0'}
+    headers = {'User-Agent': 'My User Agent 1.0'}
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         return response.json().get('data') \
