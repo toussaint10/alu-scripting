6423dc48db599158c56e11fe3a20a9854e72900a
toussaint10 <l.tona@alustudent.com>
2024-04-06 07:38:06 -0700

    API Advanced

--- api_advanced/tests/0-subs.py
+++ api_advanced/tests/0-subs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+ #!/usr/bin/python3
 """ 0-subs.py """
 import requests
 
--- api_advanced/tests/1-top_ten.py
+++ api_advanced/tests/1-top_ten.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-""" 1-top_ten.py """
+"""" Top Ten Limit"""
 import requests
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    """"top ten"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
+        . format(subreddit)
+
+    res = requests.get(url,
+                       headers={
+                           'User-Agent': 'Mozilla/5.0'})
+
+    if res.status_code != 200:
         print(None)
-        return
-    posts = response.json()['data']['children']
-    for post in posts:
-        print(post['data']['title'])
+    else:
+        json_response = res.json()
+        posts = json_response.get('data').get('children')
+        [print(post.get('data').get('title')) for post in posts]
